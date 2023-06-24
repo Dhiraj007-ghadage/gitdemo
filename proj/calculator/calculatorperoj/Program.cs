21a585bb6f9ed52788d35858d692dc65ae73fbb8
dhiraj007mayur <ghadagedhiraj007@gmail.com>
2023-06-24 20:09:15 +0530

    new calcy added

--- proj/calculator/calculatorperoj/Program.cs
+++ proj/calculator/calculatorperoj/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using calculatorperoj;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("ddg");
 Console.ReadLine();
+
+Calcy c = new Calcy();
+c.Print();
